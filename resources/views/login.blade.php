d9457d8837f3e2af04c1e2e1dda76fb5e2f46843
sonng147202 <sonnguyen20140720@gmail.com>
2022-03-23 09:02:46 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đăng nhập</title>
+</head>
+<body>
+    <h1>Đây là đăng nhập nè</h1>
+    <a href="{{route('home')}}">Sang trang chủ đi</a>
+</body>
+</html>
